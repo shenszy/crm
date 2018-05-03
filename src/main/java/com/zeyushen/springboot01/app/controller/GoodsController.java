48eaa9a7fb08ac8b986af209359de27912c3fcf4
董文强 <826112946@qq.com>
2018-05-03 14:01:34 +0800

    aa

--- src/main/java/com/zeyushen/springboot01/app/controller/GoodsController.java
+++ src/main/java/com/zeyushen/springboot01/app/controller/GoodsController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -45,5 +46,9 @@ public class GoodsController {
     public boolean add(GoodsPojo goodsPojo) {
         return  goodsService.add(goodsPojo);
     }
-
+    @PostMapping("/elsedeGoods")
+    @ResponseBody
+    public boolean delete(String gId) {
+        return  false;
+    }
 }
