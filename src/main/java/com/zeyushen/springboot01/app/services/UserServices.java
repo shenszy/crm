b905f207566590ad5d5f69c82fb35235d665c10e
董文强 <826112946@qq.com>
2018-05-03 15:40:54 +0800

    登录

--- src/main/java/com/zeyushen/springboot01/app/services/UserServices.java
+++ src/main/java/com/zeyushen/springboot01/app/services/UserServices.java
@@ -1,5 +1,6 @@
 package com.zeyushen.springboot01.app.services;
 
+import com.zeyushen.springboot01.app.mapper.UserMapper;
 import com.zeyushen.springboot01.app.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,15 +19,24 @@ import org.springframework.stereotype.Service;
 public class UserServices implements UserDetailsService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServices.class);
     @Autowired
-    BCryptPasswordEncoder passwordEncoder;
-
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private UserMapper userMapper;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         LOGGER.info("用户登录： {}",username);
-        User user = new User();
-        user.setuName(username);
-        user.setuPassword(passwordEncoder.encode("123"));
-        user.setuRole("大王");
+        User user ;
+        try {
+            user =userMapper.selectByUserName(username);
+            if(user == null){
+                throw new UsernameNotFoundException("用户登录异常");
+            }
+            if(user.getuName()==null||user.getuName().isEmpty()){
+                throw new UsernameNotFoundException("用户登录异常");
+            }
+        }catch (Exception e){
+            throw new UsernameNotFoundException("用户名不正确");
+        }
         return user;
     }
 }
