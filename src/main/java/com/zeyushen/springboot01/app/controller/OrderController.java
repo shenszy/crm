d226421debb694208ac1bfb79c0fef94909bc776
董文强 <826112946@qq.com>
2018-05-10 17:03:44 +0800

    aaa

--- src/main/java/com/zeyushen/springboot01/app/controller/OrderController.java
+++ src/main/java/com/zeyushen/springboot01/app/controller/OrderController.java
@@ -2,35 +2,65 @@ package com.zeyushen.springboot01.app.controller;
 
 import com.zeyushen.springboot01.app.model.CustomerInfoPojo;
 import com.zeyushen.springboot01.app.model.GoodsPojo;
+import com.zeyushen.springboot01.app.model.OrderInfoPojo;
 import com.zeyushen.springboot01.app.model.PactInfoPojo;
+import com.zeyushen.springboot01.app.services.GoodsService;
 import com.zeyushen.springboot01.app.services.OrderServices;
+import com.zeyushen.springboot01.app.services.UserServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
-    OrderServices orderServices;
+    private OrderServices orderServices;
+    @Autowired
+    private UserServices userServices;
+    @Autowired
+    private GoodsService goodsService;
+
 
     @RequestMapping("/add.html")
-    public ModelAndView addOrder(@RequestParam(required = true,defaultValue = "") String gId,
-                                 @RequestParam(required = true,defaultValue = "") String cId){
-        ModelAndView mv=new ModelAndView("/order/add");
-        List<GoodsPojo> product=orderServices.getProduct();
-        mv.addObject("product",product);
-        List<CustomerInfoPojo> customer=orderServices.getCustomer();
-        mv.addObject("customer",customer);
-        List<PactInfoPojo> pact=orderServices.getPact();
-        mv.addObject("pact",pact);
-        mv.addObject("gId",gId);
-        mv.addObject("cId",cId);
+    public ModelAndView addOrder(@RequestParam(required = true, defaultValue = "") String gId,
+                                 @RequestParam(required = true, defaultValue = "") String cId, Principal user) {
+
+        ModelAndView mv = new ModelAndView("/order/add");
+        List<GoodsPojo> product = orderServices.getProduct();
+        mv.addObject("product", product);
+        List<CustomerInfoPojo> customer = orderServices.getCustomer();
+        mv.addObject("customer", customer);
+        List<PactInfoPojo> pact = orderServices.getPact();
+        mv.addObject("pact", pact);
+        mv.addObject("gId", gId);
+        mv.addObject("cId", cId);
+        mv.addObject("orderCreator", userServices.getName(user.getName()));
         return mv;
+    }
+
+    @PostMapping("/add")
+    @ResponseBody
+    public boolean addOrder(OrderInfoPojo order) {
+
+        return false;
+    }
+
+    @GetMapping("/selectPrice")
+    @ResponseBody
+    public GoodsPojo selectPrice(String gId) {
+        LOGGER.info(gId);
+        GoodsPojo goodsPojo = goodsService.getById(gId);
+        return goodsPojo;
+        // return;
     }
 
 }
---
+++ src/main/java/com/zeyushen/springboot01/app/controller/UserManageController.java
@@ -0,0 +1,74 @@
+package com.zeyushen.springboot01.app.controller;
+
+import com.zeyushen.springboot01.app.model.User;
+import com.zeyushen.springboot01.app.services.StaffServices;
+import com.zeyushen.springboot01.app.services.UserServices;
+import com.zeyushen.springboot01.app.util.PagingUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 董文强
+ * @Time 2018/5/10 14:33
+ */
+@Controller
+@RequestMapping("/userManage")
+public class UserManageController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserManageController.class);
+    @Autowired
+    private StaffServices staffServices;
+    @Autowired
+    private UserServices userServices;
+
+    @RequestMapping("/user.html")
+    public ModelAndView index(@RequestParam(defaultValue = "false") Boolean onlyData,
+                              @RequestParam(defaultValue = "1") Integer pageNum) {
+      // List<Map<String,Object>> mapList =  userServices.getAll();
+        ModelAndView mv = new ModelAndView("/filemanagement/userManage/user");
+
+        PagingUtil.paging("allUser", mv, pageNum, onlyData, () -> userServices.getAll());
+
+        return mv;
+    }
+
+    @GetMapping("/addUser.html")
+    public ModelAndView addHtml() {
+        ModelAndView mv = new ModelAndView("/filemanagement/userManage/addUser");
+        mv.addObject("staffs",staffServices.getAllStaff());
+        return mv;
+    }
+
+    @PostMapping("/addUser")
+    @ResponseBody
+    public boolean add(User user) {
+        return userServices.addUser(user);
+    }
+
+    @GetMapping("/update.html")
+    public ModelAndView updateHtml(Integer id) {
+
+        User user = userServices.getUser(id);
+        ModelAndView mv = new ModelAndView("/filemanagement/userManage/updateUser");
+        mv.addObject("user",user);
+        mv.addObject("staffs",staffServices.getAllStaff());
+
+        return mv;
+    }
+    @PostMapping("/update")
+    @ResponseBody
+    public boolean update(User user) {
+        return  userServices.alert(user);
+        }
+
+    @GetMapping("/delete")
+    public ModelAndView delete() {
+        return new ModelAndView("/filemanagement/userManage/updateUser");
+    }
+}
