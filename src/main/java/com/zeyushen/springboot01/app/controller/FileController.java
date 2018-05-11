f5a389b4ed427c28233238c0d8bf70f5cb33d6ee
shenszy <13572085862@163.com>
2018-05-11 17:57:29 +0800

    11

--- src/main/java/com/zeyushen/springboot01/app/controller/FileController.java
+++ src/main/java/com/zeyushen/springboot01/app/controller/FileController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +16,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URLEncoder;
 
 @Controller
 @RequestMapping("/file")
 public class FileController {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
 
-
+    @Value("${fileForTarget}")
+    private  String fileDir ;
 
     @Autowired
     private FileServices fileService;
@@ -37,8 +42,19 @@ public class FileController {
 
     //下载
     @RequestMapping("/download")
-    public void downloadFile(HttpServletRequest request, HttpServletResponse response, String filePath, String fileName){
-        fileService.downloadFile(request,response,filePath,fileName);
+    public void downloadFile(HttpServletResponse response,String filePath,String fileName) {
+        try {
+            response.setHeader("Content-Disposition", "attachment;filename="+URLEncoder.encode(fileName, "UTF-8"));
+            response.setHeader("Connection", "close");
+            response.setHeader("Content-Type", "application/octet-stream");
+            OutputStream outputStream = response.getOutputStream();
+            filePath=fileDir+filePath;
+            filePath=filePath.replace("/file/template","/template");
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            FileCopyUtils.copy(fileInputStream,outputStream);
+        }catch (Exception e){
+            return;
+        }
     }
 
 
