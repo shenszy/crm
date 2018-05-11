0bad95471b8064272e7e0595b3157aaffdeaa044
董文强 <826112946@qq.com>
2018-05-11 09:47:19 +0800

    aaa

--- src/main/java/com/zeyushen/springboot01/app/config/WebSecurityConfig.java
+++ src/main/java/com/zeyushen/springboot01/app/config/WebSecurityConfig.java
@@ -6,13 +6,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 import sun.security.provider.MD5;
 
 /**
@@ -20,7 +26,7 @@ import sun.security.provider.MD5;
  * @Time 2018/5/3 14:12
  */
 @Configuration
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSecurityConfig.class);
 
     @Autowired
@@ -34,9 +40,9 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(
                         /*不拦截的页面*/
-                        "/file/img/**", "/fonts/**.**", "error/**","/images/**",
+                        "/file/img/**", "/fonts/**.**", "error/**", "/images/**",
                         "/css/**", "/js/**",
-                         "/favicon.ico"
+                        "/favicon.ico"
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login.html")
@@ -48,15 +54,34 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 
 
     }
+
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return  new BCryptPasswordEncoder();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
     //设置登录规则
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder).init(auth );
-
-
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder).init(auth);
     }
+
+
+
+
+
+    /****************去除权限验证的前缀********************/
+    @Bean
+    public SecurityExpressionHandler<FilterInvocation> webExpressionHandler() {
+        DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler = new DefaultWebSecurityExpressionHandler();
+        defaultWebSecurityExpressionHandler.setDefaultRolePrefix("");
+        return defaultWebSecurityExpressionHandler;
+    }
+
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+    }
+
+
 }
